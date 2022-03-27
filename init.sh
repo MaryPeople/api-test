55db43341173b74de4719e72be9153123bc96802
rhee519 <rhee519@gmail.com>
2022-03-28 00:09:43 +0900

    Firebase 호스팅 설정

--- init.sh
+++ init.sh
@@ -1,2 +1,5 @@
-# Google 서비스 계정 승인을 위한 환경 변수 설정
+# Firebase 서비스 계정 승인을 위한 환경 변수 설정
 export GOOGLE_APPLICATION_CREDENTIALS="./mary-people-test-firebase-adminsdk-d42xf-05ce444e7e.json"
+
+# Firebase CLI로 API서버를 배포하기 위한 토큰 설정
+# export FIREBASE_TOKEN="1//0e8W1OtvrRO7UCgYIARAAGA4SNwF-L9IrzGcta2b74p1kvnbFzVO_4O1-gyqWO4k564wNkYsZvGCsTkVh9YuY3C2jFbgCpQHGmC8"
